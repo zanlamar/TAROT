1f84b7a94412d30487af347f4d0173fb70b06c25
Ana Laf <zanlamar@gmail.com>
2025-07-15 16:13:36 +0200

    call to the API done + styles reorganized: background + basics

--- build/index.js
+++ build/index.js
@@ -1,4 +1,26 @@
 "use strict";
-const message = "Hello, World!";
-console.log(message);
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById("getCard");
+    const tarotCardResponse = document.getElementById("responseContainer");
+    button.addEventListener("click", getCard);
+    function getCard() {
+        fetch('https://tarotapi.dev/api/v1/cards/random?n=1')
+            .then(res => res.json())
+            .then(data => {
+            console.log(data);
+            showCard(data);
+        });
+    }
+    function showCard(data) {
+        const card = data.cards[0];
+        const cardTitle = document.createElement('h2');
+        cardTitle.textContent = card.name;
+        const cardDescription = document.createElement('p');
+        cardDescription.textContent = card.meaning_up;
+        const div = document.createElement('div');
+        div.appendChild(cardTitle);
+        div.appendChild(cardDescription);
+        tarotCardResponse.appendChild(div);
+    }
+});
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -5,12 +5,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById("getCard");
     const tarotCardResponse = document.getElementById("responseContainer");
 
-    // button.addEventListener("click", getCard);
+    button.addEventListener("click", getCard);
 
     function getCard() {
-        console.log("Botón clicado, haciendo fetch...");
         fetch('https://tarotapi.dev/api/v1/cards/random?n=1')
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => {
+            console.log(data);
+            showCard(data);
+        });
+    }
+
+    function showCard(data: object) {
+
+        const card = data.cards[0];
+
+        const cardTitle = document.createElement('h2');
+        cardTitle.textContent = card.name;
+
+        const cardDescription = document.createElement('p');
+        cardDescription.textContent = card.meaning_up;
+
+        const div = document.createElement('div');
+        div.appendChild(cardTitle);
+        div.appendChild(cardDescription);
+
+        tarotCardResponse.appendChild(div);
     }
 });
