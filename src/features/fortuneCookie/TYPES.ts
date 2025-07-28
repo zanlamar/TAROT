9aa25c14cd065c62cbb907e02dc3aeb98080afd0
Ana Laf <zanlamar@gmail.com>
2025-07-28 12:46:40 +0200

    changed typescript null values

--- src/features/fortuneCookie/TYPES.ts
+++ src/features/fortuneCookie/TYPES.ts
@@ -8,3 +8,19 @@ export interface fortuneMessage {
 
 }
 
+export type FortuneType = 
+  | { type: "tarot", card: TarotCard }
+  | { type: "cookie", fortune: string };
+
+
+export interface TarotCard {
+  desc: string, 
+  meaning_rev: string,
+  meaning_up: string,
+  name: string,    
+  name_short: string,
+  type: string,
+  value: string,
+  value_int: number
+};
+
